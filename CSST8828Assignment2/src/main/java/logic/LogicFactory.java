adcec67402474f8d9363c253bbc6dea8c5a93d32
mnewp0006 <newp0006@algonquinlive.com>
2021-03-25 11:01:32 -0500

    Committing initial LogicFactory

--- CSST8828Assignment2/src/main/java/logic/LogicFactory.java
+++ CSST8828Assignment2/src/main/java/logic/LogicFactory.java
@@ -1,5 +1,9 @@
 package logic;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+
 public abstract class LogicFactory {
 
     private static final String PACKAGE = "logic.";
@@ -8,9 +12,29 @@ public abstract class LogicFactory {
     private LogicFactory() {
     }
 
-    //TODO this code is not complete, it is just here for sake of programe working. need to be changed ocmpletely
-    public static < T> T getFor( String entityName ) {
-        //this casting wont be needed.
-        return (T)new AccountLogic();
+    // I think the correct way is: return getFor(type). 
+    public static< T> T getFor( String entityName ) {
+       
+        try {
+        Class<T> type = (Class <T>) Class.forName(PACKAGE + entityName + SUFFIX);
+        T newInstance = getFor(type);
+        return newInstance;
+        } catch (ClassNotFoundException e) {
+        }
+        return null; 
+    }
+    
+    //again probably should just return declaredConstructor
+    public static <T> T getFor(Class<T> type) {
+        
+        try {
+        Constructor <T> declaredConstructor = type.getDeclaredConstructor();
+        T newInstance = declaredConstructor.newInstance();
+        return newInstance;
+        }
+        catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | 
+                NoSuchMethodException | SecurityException e) {
+        }
+        return null;
     }
 }
